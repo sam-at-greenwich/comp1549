0ac5b8e3faa16e02ea559dcc4a397e5941a26815
sam-at-greenwich <98970326+sam-at-greenwich@users.noreply.github.com>
2024-03-20 18:05:38 +0000

    Add files via upload

---
+++ chat cw/ChatClient3.java
@@ -0,0 +1,104 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ChatClient3 {
+    String name = "temp";
+    boolean leader = false;
+    String serverAddress;
+    Scanner in;
+    PrintWriter out;
+    JFrame frame = new JFrame("Chatter");
+    JTextField textField = new JTextField(50);
+    JTextArea messageArea = new JTextArea(30, 50);
+    JButton button = new JButton("quit");
+    public ChatClient3() {
+        this.serverAddress = getName("IP (localhost for example)");
+
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, BorderLayout.CENTER);
+        frame.getContentPane().add(new JScrollPane(messageArea), BorderLayout.NORTH);
+        frame.getContentPane().add(button, BorderLayout.SOUTH);
+        frame.pack();
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                out.println("/quit");
+            }
+        });
+        textField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                out.println(textField.getText());
+                textField.setText("");
+            }
+
+        });
+    }
+    private String getName(String var) {
+        return JOptionPane.showInputDialog(
+            frame,
+            "Choose a "+ var +" :",
+            "Screen selection",
+            JOptionPane.PLAIN_MESSAGE
+        );
+    }
+
+    private void run() throws IOException {
+        try {
+            int port = Integer.parseInt(getName("port (59001 for example)"));
+            Socket socket = new Socket(serverAddress, port);
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                if (line.startsWith("SUBMITNAME")) {
+                    out.println(getName("name or ID"));
+                } else if (line.startsWith("NAMEACCEPTED")) {
+                    name = line.substring(13);
+                    this.frame.setTitle("Chatter - " + name);
+                    textField.setEditable(true);
+                } else if (line.startsWith("MESSAGE")) {
+                    messageArea.append(line.substring(8) + "\n");
+                } else if (line.startsWith("PRIVATE" + name)) {
+                messageArea.append(line.substring(7 + name.length()) + "\n");
+                } else if (line.startsWith("ROLL")) {
+                    if (!leader){
+                        out.println("HERE" + name);
+                    }
+                } else if (line.startsWith("BOSS")) {
+                    leader = true;
+                    ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+                    executor.scheduleAtFixedRate(check, 0, 20, TimeUnit.SECONDS);
+                }else if (line.startsWith("INTRO")) {
+                    out.println("hi my ID is " + name + " my IP address is "  + InetAddress.getLocalHost() + " and im talking through port "+ port);
+
+                }
+            }
+        } finally {
+            frame.setVisible(false);
+            frame.dispose();
+        }
+    }
+    Runnable check = new Runnable() {
+        public void run() {
+            out.println("HERE"+ name);
+        }
+    };
+
+    public static void main(String[] args) throws Exception {
+        ChatClient3 client = new ChatClient3();
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run();
+    }
+}
---
+++ chat cw/ChatServer.java
@@ -0,0 +1,134 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class ChatServer {
+    private static Set<String> names = new HashSet<>();
+    private static Set<String> checked = new HashSet<>();
+    static volatile List<String> convo = new ArrayList<String>();
+    static volatile  String leader = "";
+    private static Set<PrintWriter> writers = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("The group server is running on port 57001");
+        ExecutorService pool = Executors.newFixedThreadPool(500);
+        try (ServerSocket listener = new ServerSocket(59001)) {
+            while (true) {
+                pool.execute(new Handler(listener.accept()));
+            }
+        }
+    }
+    private static class Handler implements Runnable {
+        private String name;
+        private Socket socket;
+        private Scanner in;
+        private PrintWriter out;
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+        public void speak(String line) {
+            for (PrintWriter writer : writers) {
+                writer.println(line);
+            }
+            convo.add(line);
+        }
+
+        public void run() {
+            try {
+                in = new Scanner(socket.getInputStream());
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.nextLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!name.isEmpty() && !names.contains(name)) {
+                            names.add(name);
+                            checked.add(name);
+                            break;
+                        }
+                    }
+                }
+
+                out.println("NAMEACCEPTED " + name);
+                speak("MESSAGE " + name + " has joined");
+                if (leader.isEmpty()) {
+                    leader = name;
+                    speak("MESSAGE " + name + " has been appointed group  coordinator");
+                    out.println("BOSS");
+                }
+                for (String mesg : convo) {
+                    out.println(mesg);
+                }
+                writers.add(out);
+                while (true) {
+                    String input = in.nextLine();
+                    if (leader.isEmpty()) {
+                        leader = name;
+                        speak("MESSAGE " + name + " has been appointed group  coordinator");
+                        out.println("BOSS");
+                    }
+                    if (input.toLowerCase().startsWith("/quit")) {
+                        return;
+                    }
+                    if (input.toLowerCase().startsWith("/msg")) {
+                        boolean found = false;
+                        for (String test : names) {
+                            if (input.contains(test)){
+                                speak("PRIVATE" + test +name +" whispered to you "+ input.substring(5 + test.length()));
+                            found = true;
+                            break;
+                            }
+                        }
+                        if (!found){
+                            out.println("couldn't find who you were looking for");
+                        }
+                    } else if (input.startsWith("HERE")) {
+                        checked.remove(input.substring(4));
+                        if (name == leader) {
+                            for (String absent : checked) {
+                                speak("MESSAGE " + absent + " did not check in");
+                            }
+                            checked = names;
+                            speak("PRIVATE" + leader + "check complete, try /msg <name> or /req");
+                            speak("ROLL");
+                        }
+
+                    } else if (input.startsWith("/req")) {
+                        if (name == leader){
+                            speak("INTRO");
+                        }
+                    } else {
+                        LocalDateTime currentDateTime = LocalDateTime.now();
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+                        speak("MESSAGE " + name + ": " + input + " (" + formatter.format(currentDateTime) + ")");
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                if (out != null) {
+                    writers.remove(out);
+                }
+                if (leader == name){
+                    leader = "";
+                }
+                if (name != null) {
+                    System.out.println(name + " is leaving");
+                    names.remove(name);
+                    checked.remove(name);
+                    speak("MESSAGE " + name + " has left");
+                }
+                try { socket.close(); } catch (IOException e) {}
+            }
+        }
+    }
+}
